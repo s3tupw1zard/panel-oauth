5aa2d1be9c572d73986795621955eab9a9fc6034
s3tupw1zard <setupwizard@proton.me>
2024-05-18 13:44:13 +0200

    Try to fix 500 Server Error

--- routes/admin.php
+++ routes/admin.php
@@ -70,7 +70,7 @@ Route::group(['prefix' => 'settings'], function () {
     Route::get('/advanced', [Admin\Settings\AdvancedController::class, 'index'])->name('admin.settings.advanced');
 
     Route::post('/mail/test', [Admin\Settings\MailController::class, 'test'])->name('admin.settings.mail.test');
-    Route::get('/oauth', 'Settings\OAuthController@index')->name('admin.settings.oauth');
+    Route::get('/oauth', 'Admin\Settings\OAuthController@index')->name('admin.settings.oauth');
 
     Route::patch('/', [Admin\Settings\IndexController::class, 'update']);
     Route::patch('/mail', [Admin\Settings\MailController::class, 'update']);
