d6d0a70318a046637b46b83723f411dbc9b26f11
s3tupw1zard <setupwizard@proton.me>
2024-05-19 10:48:13 +0200

    Fix Routes

--- routes/base.php
+++ routes/base.php
@@ -9,8 +9,8 @@ Route::get('/account', [Base\IndexController::class, 'index'])
     ->withoutMiddleware(RequireTwoFactorAuthentication::class)
     ->name('account');
 
-Route::get('/account/oauth/link', 'OAuthController@link')->name('account.oauth.link');
-Route::get('/account/oauth/unlink', 'OAuthController@unlink')->name('account.oauth.unlink');
+Route::get('/account/oauth/link', [Base\OAuthController::class, 'link'])->name('account.oauth.link');
+Route::get('/account/oauth/unlink', [Base\OAuthController::class, 'unlink'])->name('account.oauth.unlink');
 
 Route::get('/locales/locale.json', Base\LocaleController::class)
     ->withoutMiddleware(['auth', RequireTwoFactorAuthentication::class])
